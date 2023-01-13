aca5cba1176aa399400c0b5a47e4e3b923aeed64
Avazart <avazart7@gmail.com>
2023-01-13 15:38:11 +0200

    Update DatabaseManager.py

--- DatabaseManager.py
+++ DatabaseManager.py
@@ -9,8 +9,8 @@ import settings
 async def update_count(id: int):
     async with aiosqlite.connect(settings.DATABASE_STATISTICS) as db:
         c = await db.cursor()
-        await c.execute("SELECT count FROM statistic WHERE id=?", (id,))
-        count = await c.fetchone()
+        await c.execute("SELECT count FROM statistic WHERE id=?", (id,))  # Що таке count ? count це зарезервоване ім'я ф-ці в самому SQL, невже нема проблем?
+        count = await c.fetchone() # А якщо нічого нема по такому id ?
         await c.execute("UPDATE statistic SET count=? WHERE id=?", (count[0] + 1, id))
         await db.commit()
 
@@ -55,18 +55,16 @@ async def get_statistic():
         return columns
 
 
-async def search_id(id: int):
+async def statistic_exists(id: int) -> bool :  # naming
     async with aiosqlite.connect(settings.DATABASE_STATISTICS) as db:
         c = await db.cursor()
 
         await c.execute("SELECT * FROM statistic WHERE id=?", (id,))
         result = await c.fetchone()
 
-        if result is None:
-            return False
-        else:
-            return True
-
+  
+        return result is None:
+        
 
 async def clear_statistic_table():
     async with aiosqlite.connect(settings.DATABASE_STATISTICS) as db:
@@ -97,3 +95,7 @@ async def get_hours():
         await c.execute("SELECT * FROM statistic_hour ORDER BY hour DESC")
         result = await c.fetchall()
         return result
+
+ # Що трапится якщо треба буде виклати декілька ф-цій по черзі, підряд ? Буде відкриватись закривитись з'єднання постійно?  
+    
+    
