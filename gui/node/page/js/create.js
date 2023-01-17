99ac9f383297b43a2abee85d5f58ca71748c2fec
Mark-Reshetov <114291264+Mark-Reshetov@users.noreply.github.com>
2023-01-18 01:16:58 +0200

    edit send req in js

--- gui/node/page/js/create.js
+++ gui/node/page/js/create.js
@@ -1,41 +1,22 @@
 function handler(){
-var token = document.getElementById("token").value;
-var chat_id_main = document.getElementById("chat_id_main").value;
-var chat_url_main = document.getElementById("chat_url_main").value;
-var chat_id_offtopic = document.getElementById("chat_id_offtopic").value;
-var chat_url_offtopic = document.getElementById("chat_url_offtopic").value;
-
-var path_session = document.getElementById("path_session").value;
-var path_json = document.getElementById("path_json").value;
-var path_db = document.getElementById("path_db").value;
-
-var webhook = document.getElementById("webhook").value;
-var statistics = document.getElementById("statistics").value;
-var note_user = document.getElementById("note_user").value;
-var database_auto = document.getElementById("database_auto").value;
-
-var webhook_path = document.getElementById("webhook_path").value;
-var webhook_url = document.getElementById("webhook_url").value;
-var webhook_host = document.getElementById("webhook_host").value;
-var webhook_post = document.getElementById("webhook_post").value;
 
 var url = '/send&' + '?' +
-'token=' + token + '?' +
-'chat_id_main=' + chat_id_main + '?' +
-'chat_url_main=' + chat_url_main +  '?' +
-'chat_id_offtopic=' + chat_id_offtopic + '?' +
-'chat_url_offtopic=' + chat_url_offtopic + '?' +
-'path_session=' + path_session + '?' +
-'path_json=' + path_json + '?' +
-'path_db=' + path_db + '?' +
-'webhook=' + webhook + '?' +
-'statistics=' + statistics + '?' +
-'note_user=' + note_user + '?' +
-'database_auto=' + database_auto + '?' +
-'webhook_path=' + webhook_path + '?' +
-'webhook_url=' + webhook_url + '?' +
-'webhook_host=' + webhook_host + '?' +
-'webhook_post=' + webhook_post
+'token=' + document.getElementById("token").value + '?' +
+'chat_id_main=' + document.getElementById("chat_id_main").value + '?' +
+'chat_url_main=' + document.getElementById("chat_url_main").value +  '?' +
+'chat_id_offtopic=' + document.getElementById("chat_id_offtopic").value + '?' +
+'chat_url_offtopic=' + document.getElementById("chat_url_offtopic").value + '?' +
+'path_session=' + document.getElementById("path_session").value + '?' +
+'path_json=' + document.getElementById("path_json").value + '?' +
+'path_db=' + document.getElementById("path_db").value + '?' +
+'webhook=' + document.getElementById("webhook").checked + '?' +
+'statistics=' + document.getElementById("statistics").checked + '?' +
+'note_user=' + document.getElementById("note_user").checked + '?' +
+'database_auto=' + document.getElementById("database_auto").checked + '?' +
+'webhook_path=' + document.getElementById("webhook_path").value + '?' +
+'webhook_url=' + document.getElementById("webhook_url").value + '?' +
+'webhook_host=' + document.getElementById("webhook_host").value + '?' +
+'webhook_post=' + document.getElementById("webhook_post").value;
 
 const xhr = new XMLHttpRequest();
 
